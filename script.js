286443a20cae5f14393bd55fded847a3ce8df791
jahidIqbalBiswas <official.jahidbiswas@gmail.com>
2024-01-02 19:01:08 +0600

    clock

---
+++ script.js
@@ -0,0 +1,13 @@
+function clock() {
+    let date = new Date();
+    let hour = date.getHours()
+    let minute = date.getMinutes()
+    let second = date.getSeconds()
+    let ampm = hour >= 12 ? 'pm' : "am";
+    hour = date.getHours() % 12
+    document.getElementById("hour").innerText = hour;
+    document.getElementById("minute").innerText = minute;
+    document.getElementById("second").innerText = second;
+    document.getElementById("ampm").innerText = ampm;
+}
+setInterval(clock,1000)
